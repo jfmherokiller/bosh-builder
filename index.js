3cce88c81c61f48ec57646b3c2c52c81554fbd43
noah the goodra <peterpan0413@live.com>
2018-02-02 11:03:03 -0600

    convert index to a general entrypoint

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-const fs = require("fs");
-const cheerio = require("cheerio");
-const path = require("path");
-const isValid = require("is-valid-path");
-
-function produceMapping(StringContainingSVGContents) {
-
-}
-
-function LoadSVGFile(SvgPathString) {
-    return fs.readFileSync(SvgPathString, {encoding: "utf8"});
-}
-
-function main(PathOrSvgString) {
-    let svgDoc = "";
-
-    if (CheckIfValidPath(PathOrSvgString)) {
-        svgDoc = CreateCheerioInstance(LoadSVGFile(PathOrSvgString));
-    } else {
-        svgDoc = CreateCheerioInstance(PathOrSvgString);
+const spriteMappings = require("./spriteSheetMappings");
+const generateBBox = require("./generateBoundingBoxes");
+/**
+ *
+ * @param Svgstring
+ * @param documentImplementation
+ */
+function entrypoint(Svgstring,documentImplementation) {
+    const SpriteMap = spriteMappings.MappingFunction(Svgstring,documentImplementation);
+    const BoundingBox = generateBBox(documentImplementation);
+    const fabcanvas = new fabric.StaticCanvas(null,{ width: 200, height: 200 });
+    return {
+        SpriteMaps:SpriteMap,
+        boundingBox:BoundingBox
     }
-    console.log(svgDoc);
 }
+module.exports = entrypoint;
 
-function CreateCheerioInstance(SVGString) {
-    return cheerio.load(SVGString,{xmlMode:true});
-}
 
-main("./bosh-sprite.svg");
-
-function CheckIfValidPath(StringToCheck) {
-    return isValid(StringToCheck);
-}
 
